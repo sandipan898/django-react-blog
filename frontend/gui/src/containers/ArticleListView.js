026a47ef9776b5bfb8f14517bafafa9f0e96f545
sandipan898 <sandipan.das898@gmail.com>
2020-12-15 16:53:06 +0530

    adding Article layout and components

---
+++ frontend/gui/src/containers/ArticleListView.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Article from '../components/Article';
+
+export default class ArticleList extends Component {
+    render() {
+        return (
+            <div>
+                <Article />
+            </div>
+        )
+    }
+}
